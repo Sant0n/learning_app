185133d28a953dc50ed036409ca935054280ed15
Nicole G <nicolegp94@gmail.com>
2021-03-30 21:21:00 +0300

    Parcelize and Parcelable dependecies and inclusion at Contact data class

--- app/src/main/java/nnar/learning_app/domain/model/Contact.kt
+++ app/src/main/java/nnar/learning_app/domain/model/Contact.kt
@@ -1,5 +1,13 @@
 package nnar.learning_app.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import androidx.annotation.DrawableRes
 
-data class Contact(val id: Int, val name: String, val phoneNumber: String, @DrawableRes val image: Int, val email:String)
+@Parcelize
+data class  Contact(
+    val id: Int,
+    val name: String,
+    val phoneNumber: String,
+    @DrawableRes val image: Int,
+    val email:String): Parcelable
