2deb49fe8237a2d721681e674988c2b3315cda4c
Nathan Adolfo Consuegra Rengifo <nathan.consuegrarengifo@telefonica.com>
2021-04-11 12:58:14 +0200

    Remove register functionality

--- app/src/main/java/nnar/learning_app/domain/usecase/LoginUserUsecase.kt
+++
@@ -1,8 +0,0 @@
-package nnar.learning_app.domain.usecase
-
-import nnar.learning_app.data.repository.UserRepository
-
-class LoginUserUsecase(private val userRepository: UserRepository) {
-
-    fun loginUser(email: String, password: String) = userRepository.loginUser(email, password)
-}
